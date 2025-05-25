bc23e5ff46b60a58a267c240ceadead535d3ea54
Saketh <saketh@gsstech.in>
2025-05-25 21:38:31 +0530

    Added get by id endpoint

--- GitFlowPractice/Controllers/AccountController.cs
+++ GitFlowPractice/Controllers/AccountController.cs
@@ -22,5 +22,18 @@ namespace GitFlowPractice.Controllers
                 return Ok(user);
             }
         }
+
+        [HttpGet]
+        [Route("GetUserById/{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            // Simulate getting user by ID
+            if (id <= 0)
+            {
+                return BadRequest("Invalid user ID");
+            }
+            var user = new { Id = id, Name = "John Doe" }; // Simulated user data
+            return Ok(user);
+        }
     }
 }
