8c94e2d1b1466649d47f572ab9cd19dccc366a08
Saketh <saketh@gsstech.in>
2025-05-25 21:25:55 +0530

    Added controllers

---
+++ GitFlowPractice/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitFlowPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetUser")]
+        public IActionResult GetUser()
+        {
+            // Simulate getting user data
+            var user = new { Id = 1, Name = "John Doe" };
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+            else
+            {
+                return Ok(user);
+            }
+        }
+    }
+}
